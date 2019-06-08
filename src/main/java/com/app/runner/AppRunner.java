5912ac3b21954b7fd1bb059e8936465b8692398d
Ashutosh <Ashutosh@192.168.0.10>
2019-06-09 03:31:30 +0530

    4th Spring Boot Application

---
+++ src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,31 @@
+package com.app.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+	
+	@Value("${my.message}")
+	private String msg;
+	@Value("${my.code}")
+	private String code;
+	@Value("${my.mode}")
+	private String mode;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(this);
+
+	}
+
+	@Override
+	public String toString() {
+		return "AppRunner [msg=" + msg + ", code=" + code + ", mode=" + mode + "]";
+	}
+	
+
+}
